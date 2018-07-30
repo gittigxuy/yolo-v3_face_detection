7bb6dca6f1b5669265293ec4d823816a795367c5
gittigxuy <18969043167@163.com>
2018-07-31 01:44:29 +0800

    视频检测文件

---
+++ yolo3_predict_video.py
@@ -0,0 +1,76 @@
+# -*- coding:utf-8 -*- 
+__author__ = 'xuy'
+
+'''
+基于视频的人脸检测
+usage:
+python yolo_video.py [video_path] [output_path(optional)]
+
+'''
+import colorsys
+import os
+import sys
+from timeit import default_timer as timer
+
+import numpy as np
+from keras import backend as K
+from keras.models import load_model
+from keras.layers import Input
+from PIL import Image, ImageFont, ImageDraw
+
+from yolo3.model import yolo_eval, yolo_body, tiny_yolo_body
+from yolo3.utils import letterbox_image
+if len(sys.argv) < 2:
+    print("Usage: $ python {0} [video_path] [output_path(optional)]", sys.argv[0])
+    exit()
+
+from yolo3_predict_pic import YOLO
+
+def detect_video(yolo, video_path, output_path=""):
+    import cv2
+    vid = cv2.VideoCapture(video_path)
+    if not vid.isOpened():
+        raise IOError("Couldn't open webcam or video")
+    video_FourCC    = int(vid.get(cv2.CAP_PROP_FOURCC))
+    video_fps       = vid.get(cv2.CAP_PROP_FPS)
+    video_size      = (int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                        int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    isOutput = True if output_path != "" else False
+    if isOutput:
+        print("!!! TYPE:", type(output_path), type(video_FourCC), type(video_fps), type(video_size))
+        out = cv2.VideoWriter(output_path, video_FourCC, video_fps, video_size)
+    accum_time = 0
+    curr_fps = 0
+    fps = "FPS: ??"
+    prev_time = timer()
+    while True:
+        return_value, frame = vid.read()
+        image = Image.fromarray(frame)
+        image = yolo.detect_image(image)
+        result = np.asarray(image)
+        curr_time = timer()
+        exec_time = curr_time - prev_time
+        prev_time = curr_time
+        accum_time = accum_time + exec_time
+        curr_fps = curr_fps + 1
+        if accum_time > 1:
+            accum_time = accum_time - 1
+            fps = "FPS: " + str(curr_fps)
+            curr_fps = 0
+        cv2.putText(result, text=fps, org=(3, 15), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=0.50, color=(255, 0, 0), thickness=2)
+        cv2.namedWindow("result", cv2.WINDOW_NORMAL)
+        cv2.imshow("result", result)
+        if isOutput:
+            out.write(result)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    yolo.close_session()
+
+if __name__ == '__main__':
+    video_path = sys.argv[1]
+    if len(sys.argv) > 2:
+        output_path = sys.argv[2]
+        detect_video(YOLO(), video_path, output_path)#在这里调用YOLO函数，从而使用了已经训练好的
+    else:
+        detect_video(YOLO(), video_path)
